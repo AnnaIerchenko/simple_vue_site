8d54cc9fdda64d090c38e041c9de2949ee391d54
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2023-09-13 05:22:11 -0700

    add carousel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="home">
+    <Carousel />
     <MainContainer>  
-      <h1>Hooooome</h1>
     </MainContainer>
   </div>
 </template>
 
 <script>
 import MainContainer from '@/components/MainContainer.vue'
+import Carousel from '@/components/Carousel.vue';
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Home',
   components: {
     MainContainer,
+    Carousel
   }
 }
 </script>
