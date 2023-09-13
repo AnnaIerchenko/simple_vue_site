5bfa0b15702c61d8e1b1884248f81729403cf251
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2023-09-13 06:44:41 -0700

    add home_container_offering

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="home">
     <Carousel />
+    <div class="container home__offering">
+      <div class="row">
+        <div class="col-md-4 home__border">
+          <h3 class="home__offeringTitle">
+            We are offering the best Estate Details
+          </h3>
+        </div>
+        <div class="col-md-8">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus numquam, blanditiis repellat autem cumque, quaerat voluptate temporibus, voluptas consectetur vel nisi. Voluptatem, sint pariatur perspiciatis libero consectetur vitae neque ea?
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
